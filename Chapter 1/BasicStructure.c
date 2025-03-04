243fdfcfbd4ed62c52969af7abc9548495998c4f
Mkjha101 <mkjha.uietofficial@gmail.com>
2025-03-04 05:40:21 +0530

    Basic Structure of C

---
+++ Chapter 1/BasicStructure.c
@@ -0,0 +1,10 @@
+//Basic Strucutre of a C Program
+#include <stdio.h> //-------- Preprocessor Directive ---------//
+int main(){
+    /* This is Multi-Line Comment.
+        In this way, a programmer can add Multiple Lines in C/C++
+        as comment
+    */
+   printf("Finally, I\'m able to code in C and C++ using VS Code");
+   return 0; //  ';' is statement terminator and is strictly required in C/C++, and in many other languages.
+}
