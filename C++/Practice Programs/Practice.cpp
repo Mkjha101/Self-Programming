67ea3d04fa6cb46b24b5be2667ada8adf9ac6f86
Mkjha101 <mkjha.uietofficial@gmail.com>
2025-04-16 18:07:20 +0530

    Practice Programs Changes [April 16

---
+++ C++/Practice Programs/Practice.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    return 0;
+}
+
+
+// 1
+/*
+int main(){
+    int n; float m;
+    cout << "Enter n: "; cin >> n;
+    cout << "Enter m: "; cin >> m;
+    cout << "Value of n: " << n << endl;
+    cout << "Value of m: " << m << endl;
+    n*=m;
+    cout << "Value of n: " << n << endl;
+    return 0;
+}
+*/
+
+// 2: Wrong Code
+/*
+int main(){
+    int n=100;
+    int Array[n];
+    cout << "Initial Address of Array: " << &Array[0] << endl;
+    n=150;
+    int Array[n];
+    cout << "Initial Address of Array: " << &Array[0] << endl;
+    return 0;
+}
+*/
--- C/Practice Programs/Practice.c
+++ C/Practice Programs/Practice.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+int PTrial(int , int *, int *);
+int PTrial(int x, int *y, int *z){
+    printf("Again, size is: %d", x);
+    for(int i=0; i<x; i++){
+        if(i==0){
+            *y>*(y+1)?*z=1:0;
+        }
+        else if(i==x-1){
+            *y>*(y-1)?*z=1:0;
+        }
+        else{
+            (*y>*(y+1) && *y>*(y-1))?*z=1:0;
+        }
+        y++;
+        z++;
+    }
+}
+
 int main(){
     return 0;
 }
@@ -29,3 +47,42 @@ int main(){
 	return 0;
 }
 */
+
+
+// 3
+/*
+int main(){
+    int a=1;
+    printf("%d %d %d", a, ++a, a++);
+    return 0;
+}
+*/
+
+//4
+/*
+int main(){
+    int num=5;
+    int a[num], Peaks[num];
+    for(int i=0; i<num; i++){
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &a[i]);
+        Peaks[i]=0;
+    }
+    printf("Size of Array \'a\' is: %d\n", num);
+    PTrial(num, &a[0], &Peaks[0]);
+
+    printf("\nUser Array: { ");
+    for(int i=0; i<num; i++){
+        printf("%d ", a[i]);
+    }
+    printf("}");
+
+    printf("\nPeaks Array: { ");
+    for(int i=0; i<num; i++){
+        printf("%d ", Peaks[i]);
+    }
+    printf("}");
+    return 0;
+}
+*/
+
